fc98c496ca9ed9a679a4567ff6b9de68012b7105
Amber Lee <lee.amber.alex@gmail.com>
2020-10-06 21:50:56 -0700

    Adjusted and removed unused pseudo code.

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 /// 1. Open planner, current time and day displays at the top.
 // 2. Time blocks start at 7AM to 6PM.
 // 3. Tasks:
-// a. If .timeblocks is equal to now time, color white.
-// If value of .timeblocks is === now, event.target to .present
-// b. If .timeblocks is less than now time, color grey.
-// If value of .timeblocks is < now, event.target to .past
-// c. If .timeblocks is greater than now time, color green.
-// If value of .timeblocks is > now, event.target to .future
+// a. If task time is equal to now time, color white.
+// b. If task time is less than now time, color grey.
+// c. If task time is greater than now time, color green.
 // 4. Click save to store to local storage.
 // 5. Refresh and data still persists.
 
@@ -25,25 +22,28 @@ console.log(moment().format("LT"));
 console.log($(inputEl));
 
 $(document).ready(function () {
+
+// 3. Tasks:
   $("input").each(function () {
     console.log(this)
-    
     var timeNow = parseInt($(this).attr("id"));
     var current = moment().hour();
     console.log(localStorage.getItem(timeNow));
       
     $(this).val(localStorage.getItem(timeNow) || "");
-
+// a. If task time is equal to now time, color white.
     if (current === timeNow) {
       $(this).addClass("present");
+// b. If task time is less than now time, color grey.
     } else if (timeNow < current) {
       $(this).addClass("past");
+// c. If task time is greater than now time, color green.
     } else {
       $(this).addClass("future");
     }
   });
 
-  // 4. Click save to store to local storage.
+// 4. Click save to store to local storage.
   $(buttonEl).on("click", function (event) {
     event.preventDefault();
    
